33946fbd76dad29e03507c0901015c15d6e95fd5
unknown <kalubekalu1@gmail.com>
2020-02-19 20:22:44 +0300

    Updates

---
+++ precache-manifest.3e261ab54162a931e8b200dbfbd6a6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ad39da485fd6564b7886fd49966d03",
+    "url": "/ReactfrontEnd/index.html"
+  },
+  {
+    "revision": "fcf6c23a6d7cad17b1b9",
+    "url": "/ReactfrontEnd/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "019093ce4dd89cbbb27c",
+    "url": "/ReactfrontEnd/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "fcf6c23a6d7cad17b1b9",
+    "url": "/ReactfrontEnd/static/js/2.ec040855.chunk.js"
+  },
+  {
+    "revision": "019093ce4dd89cbbb27c",
+    "url": "/ReactfrontEnd/static/js/main.91b1f360.chunk.js"
+  },
+  {
+    "revision": "454b086b18788f565798",
+    "url": "/ReactfrontEnd/static/js/runtime~main.3ed53593.js"
+  }
+]);
